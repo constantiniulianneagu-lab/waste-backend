b6337beb403f3d0155f30e2a1ea8f716a8d19661
constantiniulianneagu-lab <constantiniulian.neagu@gmail.com>
2025-11-18 11:05:47 +0200

    1

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,5 @@
 // src/controllers/authController.js
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';  // ✅ Nou
 import jwt from 'jsonwebtoken';
 import pool from '../config/database.js';
 
