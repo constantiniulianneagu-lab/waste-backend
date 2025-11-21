883108f1c6ed46d9b75c31ee7d156224ff6c7163
constantiniulianneagu-lab <constantiniulian.neagu@gmail.com>
2025-11-21 11:26:23 +0200

    vercel

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://waste-frontend-bw2zg5rry.vercel.app',
+    'https://waste-frontend-a49nk8os7.vercel.app',
     /\.webcontainer\.io$/,  // ✅ Permite toate subdomeniile .webcontainer.io
     /\.local-credentialless\.webcontainer\.io$/  // ✅ Specific pentru local credentialless
   ],
