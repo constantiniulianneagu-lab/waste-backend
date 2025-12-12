a513cb1276f3ccde57d380ada387f9a5ee7b9b1c
constantiniulianneagu-lab <constantiniulian.neagu@gmail.com>
2025-12-12 11:31:02 +0200

    vercel

--- src/server.js
+++ src/server.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://waste-frontend-6xmk0aqql.vercel.app',
+    'https://waste-frontend-n20bxl5lx.vercel.app',
     /\.webcontainer\.io$/,
     /\.local-credentialless\.webcontainer\.io$/
   ],
