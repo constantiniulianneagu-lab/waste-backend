587a1071ff77864092ebcc0587391d9085882bf6
constantiniulianneagu-lab <constantiniulian.neagu@gmail.com>
2026-01-28 09:33:52 +0200

    1

--- src/server.js
+++ src/server.js
@@ -33,7 +33,7 @@ app.set('trust proxy', 1);
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://waste-frontend-lrz1ra9rx.vercel.app',
+    'https://waste-frontend-8d58o8ety.vercel.app',
     /\.webcontainer\.io$/,
     /\.local-credentialless\.webcontainer\.io$/
   ],
